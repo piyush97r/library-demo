61e705e4d89fc2125b985b0d812f7aef5f62a190
piyush97r <piyush97ranjan@gmail.com>
2021-02-20 13:01:56 +0530

    Testing Added Books and Numbers

--- src/test/java/com/sessions/LibraryTest.java
+++ src/test/java/com/sessions/LibraryTest.java
@@ -1,6 +1,7 @@
 package com.sessions;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -11,19 +12,24 @@ import static org.hamcrest.Matchers.hasItem;
 import static org.junit.jupiter.api.Assertions.*;
 
 class LibraryTest {
+    Library library;
 
 
+    @BeforeEach
+    public void setup(){
+        library = new Library();
+    }
     @Test
     public void the_default_number_of_books_should_be_1(){
 
-        Library library = new Library();
+        //Library library = new Library();
         library.getBooks().size();
         int totalNumberOfBooks = library.getBooks().size();
         assertEquals(1,totalNumberOfBooks);
     }
     @Test
     public void adding_to_catalogue_should_increase_the_size_of_books_and_id_should_be_2(){
-        Library library = new Library();
+       // Library library = new Library();
         Book newlyCreatedBook = library.addToCatalogue("Discovery Of India","Jawaharlal Nehru",432,11.99);
         assertEquals(2,newlyCreatedBook.getId());
         int totalBooks = library.getBooks().size();
@@ -36,14 +42,14 @@ class LibraryTest {
 
     @Test
     public void findBookByName_called_with_bookname_should_return_book_object(){
-        Library library = new Library();
+      //  Library library = new Library();
         Book book = library.findBookByName("The God Of Small Things");
         Assertions.assertNotNull(book);
     }
 
     @Test
     public void findBookByName_called_with_nonexistant_bookname_should_return_null(){
-        Library library = new Library();
+       // Library library = new Library();
         Book book = library.findBookByName("The God of Big Things");
         assertNull(book);
     }
